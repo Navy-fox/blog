9626809a80f6b348f8223b1060b6bd8168c36287
navyFox <lina.666@list.ru>
2021-07-03 14:46:41 +0300

    Сделала шаблон страницы

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <banner img="/img/banner-river.svg" />
+    <p class="title">This is our blog</p>
+    <banner :banner="banners.top" />
+    <news-line class="wrapper blog-margin" :news="posts.slice(0, 8)" />
+    <banner class="blog-margin" :banner="banners.bottom" />
+    <news-line class="wrapper blog-margin" :news="posts.slice(8)" />
     <pagination />
   </div>
 </template>
@@ -9,9 +13,29 @@
 // @ is an alias to /src
 import Banner from "@/components/Banner";
 import Pagination from "@/components/Pagination";
+import NewsLine from "@/components/NewsLine";
+import { POSTS } from "@/data/POSTS";
+import { BANNERS } from "@/data/BANNERS";
 
 export default {
   name: "Home",
-  components: { Banner, Pagination },
+  components: { NewsLine, Banner, Pagination },
+  data() {
+    return {
+      posts: POSTS,
+      banners: BANNERS,
+    };
+  },
 };
 </script>
+
+<style lang="scss">
+.blog-margin {
+  margin-top: 120px;
+}
+.title {
+  margin-top: 53px;
+  margin-bottom: 88px;
+  margin-left: 142px;
+}
+</style>

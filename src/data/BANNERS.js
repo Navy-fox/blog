56f1f93c3015e87b318d7954abf096343254a913
navyFox <lina.666@list.ru>
2021-07-03 14:44:57 +0300

    Добавила константы BANNERS and POSTS

---
+++ src/data/BANNERS.js
@@ -0,0 +1,18 @@
+export const BANNERS = {
+  top: {
+    title: "Helping a local business reinvent itself",
+    img: "/img/banner-car.svg",
+    author: {
+      name: "Courtney Henry",
+      img: "/icon/icon30.svg",
+    },
+  },
+  bottom: {
+    title: "Helping a local business reinvent itself",
+    img: "/img/banner-river.svg",
+    author: {
+      name: "Arlene McCoy",
+      img: "/icon/icon40.svg",
+    },
+  },
+};
---
+++ src/data/POSTS.js
@@ -0,0 +1,97 @@
+export const POSTS = [
+  {
+    title: "Emmanuel Macron seeks his green image",
+    img: "/img/neon.svg",
+    size: 4,
+    author: {
+      name: "Courtney Henry",
+      img: "/icon/icon30.svg",
+    },
+  },
+  {
+    title: "Sfetas:How Erdogan instrumentalises the Ottomans",
+    img: "/img/car.svg",
+    size: 4,
+    author: {
+      name: "Jenny Wilson",
+      img: "/icon/icon31.svg",
+    },
+  },
+  {
+    title: "Criticism of Islamism: A deadly ideology",
+    img: "/img/papaya.svg",
+    size: 4,
+    author: {
+      name: "Wade Warren",
+      img: "/icon/icon32.svg",
+    },
+  },
+  {
+    title: "The Supreme Court's unused opportunity",
+    img: "/img/bay.svg",
+    size: 4,
+    author: {
+      name: "Savannah Nguyen",
+      img: "/icon/icon33.svg",
+    },
+  },
+  {
+    title:
+      "The new residence of King Juan Carlos and its judicial consequences",
+    img: "/img/village.svg",
+    size: 4,
+    author: {
+      name: "Darrell Steward",
+      img: "/icon/icon34.svg",
+    },
+  },
+  {
+    title: "Belarus: there can be only one",
+    img: "/img/girl.svg",
+    size: 4,
+    author: {
+      name: "Floyd Miles",
+      img: "/icon/icon35.svg",
+    },
+  },
+  {
+    title:
+      "10GB bundle - the most popular plan, comparison price TTM and competitors",
+    img: "/img/city.svg",
+    size: 6,
+    author: {
+      name: "Cameron Williamson",
+      img: "/icon/icon36.svg",
+    },
+  },
+  {
+    title:
+      "10GB bundle - the most popular plan, comparison price TTM and competitors",
+    img: "/img/computer.svg",
+    size: 6,
+    author: {
+      name: "Marvin McKinney",
+      img: "/icon/icon37.svg",
+    },
+  },
+  {
+    title:
+      "10GB bundle - the most popular plan, comparison price TTM and competitors",
+    img: "/img/old.svg",
+    size: 8,
+    author: {
+      name: "Brooklyn Simmons",
+      img: "/icon/icon38.svg",
+    },
+  },
+  {
+    title:
+      "10GB bundle - the most popular plan, comparison price TTM and competitors",
+    img: "/img/student.svg",
+    size: 4,
+    author: {
+      name: "Ronald Richards",
+      img: "/icon/icon39.svg",
+    },
+  },
+];

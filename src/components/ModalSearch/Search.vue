11c5d532adf3698f6a947fafd0760c522ea6304e
navyFox <lina.666@list.ru>
2021-07-12 13:17:33 +0300

    сделала таблицу для поиска

---
+++ src/components/ModalSearch/Search.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <table-search :items="items" />
+  </div>
+</template>
+
+<script>
+import TableSearch from "@/components/ModalSearch/TableSearch";
+import { SEARCH_TELECOM } from "@/data/SEARCH_TELECOM";
+export default {
+  name: "Search",
+  components: { TableSearch },
+  data() {
+    return {
+      items: SEARCH_TELECOM,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/ModalSearch/TableSearch.vue
@@ -0,0 +1,60 @@
+<template>
+  <div v-if="items">
+    <table class="table-search" rules="rows">
+      <tr class="table-search__row" v-for="(item, key) in items" :key="key">
+        <td class="table-search__text table-search__first-column">
+          {{ item.text }}
+        </td>
+        <td class="table-search__text table-search__text--light">
+          {{ item.date }}
+        </td>
+        <td
+          class="
+            table-search__text
+            table-search__text--light
+            table-search__last-column
+          "
+        >
+          {{ item.name }}
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TableSearch",
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.table-search {
+  width: 100%;
+  &__first-column {
+    width: 47.6%;
+  }
+  &__last-column {
+    width: 31.9%;
+  }
+  &__text {
+    font-family: $font, sans-serif;
+    font-size: 28px;
+    line-height: 130%;
+    color: $font-color-dark;
+    padding: 24px 0;
+    @media (max-width: $screen-laptop) {
+      font-size: 20px;
+    }
+    &--light {
+      color: $font-color-light;
+    }
+  }
+}
+</style>
